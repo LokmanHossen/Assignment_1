4b8c8e6e02efdf26f502dedc80b9e8385bfeb46f
Lokman Hossen <lokmanhossen230@gmail.com>
2025-02-23 23:22:05 +0600

    Header&SearchBar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:assignment_1/constant/colors.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,11 +12,129 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home Screen"),
-      ),
-      body: Center(
-        child: const Text("Welcome to Home Screen"),
+      backgroundColor: kBackgroundColor,
+      body: Stack(
+        children: [
+          //for items
+          SafeArea(
+            child: SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
+              scrollDirection: Axis.vertical,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //top container
+
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 30,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "hello Lokman ",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 30),
+                            ),
+                            Text(
+                              "Let's watch today",
+                              style:
+                                  TextStyle(color: Colors.grey, fontSize: 16),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                            width: 40), // Added space between text and image
+                        Stack(
+                          children: [
+                            Container(
+                              height: 50,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(30),
+                                image: const DecorationImage(
+                                  image: AssetImage("assets/actor_1.jpeg"),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              top: 10,
+                              left: 10,
+                              child: Container(
+                                height: 30,
+                                width: 30,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        // const Spacer()
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                    child: Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[800],
+                        border: Border.all(color: Colors.white70),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: const Row(
+                        children: [
+                          Expanded(
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: "Search",
+                                hintStyle: TextStyle(
+                                    color: Colors.white, fontSize: 18),
+                                border: InputBorder.none,
+                              ),
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 18),
+                            ),
+                          ),
+                          Icon(
+                            Icons.search,
+                            color: Colors.grey,
+                            size: 30,
+                          ),
+                        ],
+                      ),
+
+                      // child: const Row(
+                      //   children: [
+                      //     Text("Search",
+                      //         style:
+                      //             TextStyle(color: Colors.white, fontSize: 18)),
+                      //     // SizedBox(
+                      //     //   width: 10,
+                      //     // ),
+                      //     Spacer(),
+                      //     Icon(
+                      //       Icons.search,
+                      //       color: Colors.grey,
+                      //       size: 30,
+                      //     ),
+                      //   ],
+                      // ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
